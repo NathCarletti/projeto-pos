764adaf635b80e876cb568c8f72df90ecabb0496
Caroline Tenorio Ribeiro <caroltr.srs@gmail.com>
2017-07-23 11:18:44 -0300

    Added firebase initialization file

---
+++ js/firebase-initialization.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAcgG76Vh47saDmi2AxBtS3gd1akfKnbRg",
+    authDomain: "projeto-final-dm104.firebaseapp.com",
+    databaseURL: "https://projeto-final-dm104.firebaseio.com",
+    projectId: "projeto-final-dm104",
+    storageBucket: "projeto-final-dm104.appspot.com",
+    messagingSenderId: "244550206365"
+};
+firebase.initializeApp(config);
